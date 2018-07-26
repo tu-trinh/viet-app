44bab231f0aaf02ae718ba89699c15f2f7e45ed4
MT-GoCode <tminh.us@gmail.com>
2018-07-25 22:52:57 -0700

    TitleBar larger and covers top

--- src/pages/TitleBar.js
+++ src/pages/TitleBar.js
@@ -9,7 +9,7 @@ export default class TitleBar extends Component {
     return (
       // Style will be changed later to be dynamic
       <div>
-      <div className="TitleBar" style  = {{width:'100%', position: 'fixed', backgroundColor: this.props.color, textAlign: 'center', color: 'white'}}>
+      <div className="TitleBar" style = {{width:'100%', position: 'fixed', backgroundColor: this.props.color, textAlign: 'center', color: 'white'}}>
         <h1>{this.props.title}</h1>
       </div>
       <br/><br/><br/><br/><br/>
