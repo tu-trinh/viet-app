ba7c3d612ed6eff597d08bd0e97ec387678924cb
Tu Trinh <alinatu.trinh@gmailcom>
2018-08-02 17:33:59 -0700

    Fix decline login

--- src/utils/AuthService.js
+++ src/utils/AuthService.js
@@ -61,13 +61,17 @@ function getParameterByName(name) {
 // Get and store access_token in local storage
 export function setAccessToken() {
   let accessToken = getParameterByName('access_token');
-  localStorage.setItem(ACCESS_TOKEN_KEY, accessToken);
+  if (accessToken) {
+    localStorage.setItem(ACCESS_TOKEN_KEY, accessToken);
+  }
 }
 
 // Get and store id_token in local storage
 export function setIdToken() {
   let idToken = getParameterByName('id_token');
-  localStorage.setItem(ID_TOKEN_KEY, idToken);
+  if (idToken) {
+    localStorage.setItem(ID_TOKEN_KEY, idToken);
+  }
 }
 
 // Checks if the token is valid 
