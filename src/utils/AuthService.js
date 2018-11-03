1ddfa14444ae1e6473cc6ba14a34715740ff3c2c
Tu Trinh <alinatu.trinh@gmailcom>
2018-11-03 00:19:25 -0700

    Edit auth0

--- src/utils/AuthService.js
+++ src/utils/AuthService.js
@@ -8,7 +8,7 @@ const CLIENT_ID = 'zZPp1OHnb392igO9QoGxBnJJ2Ct57tGx';
 const CLIENT_DOMAIN = 'mt-at.auth0.com';
 const REDIRECT = 'http://vietclass.org/callback';
 const SCOPE = 'Access:app';
-const AUDIENCE = 'https://hoctiengviet.com';
+const AUDIENCE = 'https://vietclass.org';
 
 var auth = new auth0.WebAuth({
   clientID: CLIENT_ID,
