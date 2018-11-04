d4ea7a9f38cffa16ee878aaa13198125493beb8a
Tu Trinh <alinatu.trinh@gmailcom>
2018-11-04 01:44:25 -0700

    Start moving MongoDB to Heroku

--- server/models/index.js
+++ server/models/index.js
@@ -1,5 +1,7 @@
-var mongoose = require("mongoose");
-mongoose.connect("mongodb://localhost:27017/viet-app");
+const mongoose = require("mongoose");
+let uri = 'mongodb://heroku_3z5263pd:vjgmcq5q9j1s4mhvj6cjqgdoma@ds151753.mlab.com:51753/heroku_3z5263pd';
+mongoose.connect(uri);
+let db = mongoose.connection;
 
 module.exports.Book = require("./Book");
 module.exports.Lesson = require("./Lesson")
